c457085d7eed147ef371dd02fb113282f09ff075
jumorap <jumorap@unal.edu.co>
2021-05-24 09:46:57 -0500

    Updates

---
+++ precache-manifest.9fc3af4a56050fb58093ebe6fc9ec133.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e41aa32c67c741ade2833e80c1ee8a",
+    "url": "jumorap.github.io/jumorap/index.html"
+  },
+  {
+    "revision": "59579f06f6be75d641ed",
+    "url": "jumorap.github.io/jumorap/static/css/main.aebcad6b.chunk.css"
+  },
+  {
+    "revision": "8cbcd423c7c0c22b651d",
+    "url": "jumorap.github.io/jumorap/static/js/2.1eae7bdc.chunk.js"
+  },
+  {
+    "revision": "823889b60126b4363f8f",
+    "url": "jumorap.github.io/jumorap/static/js/3.e44b0f6c.chunk.js"
+  },
+  {
+    "revision": "59579f06f6be75d641ed",
+    "url": "jumorap.github.io/jumorap/static/js/main.d7cba359.chunk.js"
+  },
+  {
+    "revision": "dcdc3f80e1927cf22640",
+    "url": "jumorap.github.io/jumorap/static/js/runtime~main.25662646.js"
+  },
+  {
+    "revision": "a591f1d24061cffbe60c3af24f04ae05",
+    "url": "jumorap.github.io/jumorap/static/media/gameOfLife.a591f1d2.png"
+  },
+  {
+    "revision": "7779f4d5ea774cbbb2a46f4337faa583",
+    "url": "jumorap.github.io/jumorap/static/media/linusTorvalds.7779f4d5.png"
+  },
+  {
+    "revision": "64dc2d33af542a37754a8e733a173a85",
+    "url": "jumorap.github.io/jumorap/static/media/principalBack.64dc2d33.jpg"
+  },
+  {
+    "revision": "5a902f03b1d6a645ece828018f8e2e1f",
+    "url": "jumorap.github.io/jumorap/static/media/redBoardLogo.5a902f03.png"
+  },
+  {
+    "revision": "96abb3bb6d152689658081cb2825f85b",
+    "url": "jumorap.github.io/jumorap/static/media/salieri.96abb3bb.png"
+  },
+  {
+    "revision": "41fb5c58a62955846522eba7c3474693",
+    "url": "jumorap.github.io/jumorap/static/media/unal.41fb5c58.png"
+  }
+]);
